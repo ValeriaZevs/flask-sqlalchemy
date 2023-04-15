0a2fdb5d2a8cd64e5d359b00743f63ec3aaa930b
Zizevskikh Valeria <118841521+ValeriaZevs@users.noreply.github.com>
2023-04-15 20:56:31 +0300

    Add files via upload

---
+++ jobs_load.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from data import db_session
+from data.jobs import Jobs
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/jobs.db")
+    #app.run()
+    session = db_session.create_session()
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.is_finished = False
+    session.add(job)
+    session.commit()
+
+
+
+if __name__ == '__main__':
+    main()
